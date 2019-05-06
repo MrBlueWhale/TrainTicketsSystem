4648ceb11eec36d8e66b5dde031e477f8f2df364
ZY <ZY@ZYdeMacBook-Pro.local>
2019-05-06 15:42:36 +0800

    更新了前端代码

---
+++ train-tickets-front-end/build/precache-manifest.b4d64ee9c83cf7710bb2d2ccefa49a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194a79c910c89ccb6ce80d4c7bc32a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee4e9e37818c71c28e",
+    "url": "/static/css/main.9e5da6a2.chunk.css"
+  },
+  {
+    "revision": "30cf0af41aff06c798d0",
+    "url": "/static/js/2.0548fe37.chunk.js"
+  },
+  {
+    "revision": "39ee4e9e37818c71c28e",
+    "url": "/static/js/main.5dc9ae5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
